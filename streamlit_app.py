e0f1e27807d1864a188335ced9d83de6758b355b
justinrothaug <94494722+justinrothaug@users.noreply.github.com>
2024-08-28 13:08:52 -0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -19,27 +19,96 @@ PPLX_API_KEY= os.environ['PPLX_API_KEY']
 PPLX_API_KEY2 = "Bearer "+PPLX_API_KEY
 
 text_input = st.text_input("Enter Everlastly Product 👇", key="4", value = "Lodge 12 Inch Cast Iron Skillet")
-prompt = st.text_area("Enter Prompt 👇", key="5", value = "Follow the below steps:"+
+
+
+product_prompt = st.text_area("Product & BOM 👇", key="5", value = "Follow the below steps:"+
 "2) Find the materials used in creating the product\n"+
 "3) Find the estimated % and weight in pounds for each material.\n"+ 
 "4) Calculate the Co2 carbon footprint for each Material.\n"+ 
 "5) Multiply the percentage of each material by its respective carbon footprint per pound and then sum these values to estimate the Total Co2 for the Product.\n"+
 "Format each step with a Header and the data. Output in CSV format like this: UPC Code,Product Name,Material,Country of Origin,Estimated %,Weight in Pounds,CO2 Carbon Footprint per Pound,Total CO2")
 
-card="For the following product:"+text_input+prompt
-                        
-template = """You are a helpful assistant in giving a product description for {text}. 
-Provide a one sentence explanation """
-system_message_prompt = SystemMessagePromptTemplate.from_template(template)
-human_template = "{text}"
-human_message_prompt = HumanMessagePromptTemplate.from_template(human_template)
-chat_prompt = ChatPromptTemplate.from_messages([system_message_prompt, human_message_prompt])
-def get_chatassistant_aitopics():
-    aitopics = LLMChain(
-        llm=ChatPerplexity(model="llama-3.1-sonar-small-128k-online", temperature=.8),prompt=chat_prompt,verbose=True)
-    return aitopics
-aitopics = get_chatassistant_aitopics()
 
+category_prompt = st.text_area("Category 👇", key="5", value = "Follow the below steps:"+
+"2) Find the materials used in creating the product\n"+
+"3) Find the estimated % and weight in pounds for each material.\n"+ 
+"4) Calculate the Co2 carbon footprint for each Material.\n"+ 
+"5) Multiply the percentage of each material by its respective carbon footprint per pound and then sum these values to estimate the Total Co2 for the Product.\n"+
+"Format each step with a Header and the data. Output in CSV format like this: UPC Code,Product Name,Material,Country of Origin,Estimated %,Weight in Pounds,CO2 Carbon Footprint per Pound,Total CO2")
+
+lifespan_prompt = st.text_area("Lifespan 👇", key="5", value = "Follow the below steps:"+
+"2) Find the materials used in creating the product\n"+
+"3) Find the estimated % and weight in pounds for each material.\n"+ 
+"4) Calculate the Co2 carbon footprint for each Material.\n"+ 
+"5) Multiply the percentage of each material by its respective carbon footprint per pound and then sum these values to estimate the Total Co2 for the Product.\n"+
+"Format each step with a Header and the data. Output in CSV format like this: UPC Code,Product Name,Material,Country of Origin,Estimated %,Weight in Pounds,CO2 Carbon Footprint per Pound,Total CO2")
+
+
+
+productcard="For the following product:"+text_input+product_prompt
+categorycard="For the following product:"+text_input+category_prompt
+lifespancard="For the following product:"+text_input+lifespan_prompt
+      
+
+def ProductBOM():
+  template = """You are a helpful assistant in following instructions for {text}. 
+  Provide a one sentence explanation """
+  system_message_prompt = SystemMessagePromptTemplate.from_template(template)
+  human_template = "{text}"
+  human_message_prompt = HumanMessagePromptTemplate.from_template(human_template)
+  chat_prompt = ChatPromptTemplate.from_messages([system_message_prompt, human_message_prompt])
+  def get_chatassistant_aitopics():
+      aitopics = LLMChain(
+          llm=ChatPerplexity(model="llama-3.1-sonar-small-128k-online", temperature=.8),prompt=chat_prompt,verbose=True)
+      return aitopics
+  aitopics = get_chatassistant_aitopics()
+  responsememories = aitopics.run(productcard)
+  st.warning("Current Product:  \n"+responsememories)
+
+
+def category():
+  template = """You are a helpful assistant in following instructions for {text}. 
+  Provide a one sentence explanation """
+  system_message_prompt = SystemMessagePromptTemplate.from_template(template)
+  human_template = "{text}"
+  human_message_prompt = HumanMessagePromptTemplate.from_template(human_template)
+  chat_prompt = ChatPromptTemplate.from_messages([system_message_prompt, human_message_prompt])
+  def get_chatassistant_aitopics():
+      aitopics = LLMChain(
+          llm=ChatPerplexity(model="llama-3.1-sonar-small-128k-online", temperature=.8),prompt=chat_prompt,verbose=True)
+      return aitopics
+  aitopics = get_chatassistant_aitopics()
+  responsememories = aitopics.run(categorycard)
+  st.warning("Current Product:  \n"+responsememories)
+
+def lifespan():
+  template = """You are a helpful assistant in following instructions for {text}. 
+  Provide a one sentence explanation """
+  system_message_prompt = SystemMessagePromptTemplate.from_template(template)
+  human_template = "{text}"
+  human_message_prompt = HumanMessagePromptTemplate.from_template(human_template)
+  chat_prompt = ChatPromptTemplate.from_messages([system_message_prompt, human_message_prompt])
+  def get_chatassistant_aitopics():
+      aitopics = LLMChain(
+          llm=ChatPerplexity(model="llama-3.1-sonar-small-128k-online", temperature=.8),prompt=chat_prompt,verbose=True)
+      return aitopics
+  aitopics = get_chatassistant_aitopics()
+  responsememories = aitopics.run(lifespancard)
+  st.warning("Current Product:  \n"+responsememories)
+
+
+st.button('Run', on_click=ProductBOM, key = "123", use_container_width=True)
+st.button('Run', on_click=catgory, key = "123", use_container_width=True)
+st.button('Run', on_click=lifespan, key = "123", use_container_width=True)
+
+
+
+
+
+
+
+
+#Test output from API directly:
 def StartConvo2():
   import requests
   url = "https://api.perplexity.ai/chat/completions"
@@ -48,15 +117,6 @@ def StartConvo2():
   response = requests.post(url, json=payload, headers=headers)
   responsetext=response.text
   st.warning("Current Product:  \n"+responsetext)
-
-
-def StartConvo():
-  responsememories = aitopics.run(card)
-  st.warning("Current Product:  \n"+responsememories)
-
-
-
-st.button('Run', on_click=StartConvo, key = "123", use_container_width=True)
 #st.button('Run2', on_click=StartConvo2, key = "121", use_container_width=True)
 
 
