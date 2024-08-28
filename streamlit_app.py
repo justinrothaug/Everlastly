b76a250a86dffac919ff38c3e3a6af9af317ca82
justinrothaug <94494722+justinrothaug@users.noreply.github.com>
2024-08-28 12:11:44 -0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -28,19 +28,20 @@ prompt = st.text_area("Enter Prompt 👇", key="5", value = "Follow the below st
 
 "Format each step with a Header and the data. Output in CSV format like this: UPC Code,Product Name,Material,Country of Origin,Estimated %,Weight in Pounds,CO2 Carbon Footprint per Pound,Total CO2")
 
-card=text_input
+#card=prompt
                     
 
-template = """You are a helpful assistant in returning the Co2 for a product. Follow the directions provided:"""
+template2 = """You are a helpful assistant in returning the Co2 for a product. Follow the directions provided:"""
+template = template+prompt
 system_message_prompt = SystemMessagePromptTemplate.from_template(template)
 human_template = "{text}"
 human_message_prompt = HumanMessagePromptTemplate.from_template(human_template)
 chat_prompt = ChatPromptTemplate.from_messages([system_message_prompt, human_message_prompt])
 def get_chatassistant_aitopics():
     aitopics = LLMChain(
-        llm=ChatPerplexity(model="llama-3.1-sonar-huge-128k-online", temperature=0,max_tokens=4000),prompt=chat_prompt+prompt,verbose=True)
+        llm=ChatPerplexity(model="llama-3.1-sonar-huge-128k-online", temperature=0,max_tokens=4000),prompt=template2,verbose=True)
     return aitopics
 aitopics = get_chatassistant_aitopics()
 
-responsememories = aitopics.run(card)
+responsememories = aitopics.run(text_input)
 st.warning("Current Product:  \n"+responsememories)
