3aa401c39f3b7d00c4f72aa7a94c4170f842f268
justinrothaug <94494722+justinrothaug@users.noreply.github.com>
2024-08-28 12:04:39 -0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -33,14 +33,14 @@ card=prompt+text_input
 
 template = """You are a helpful assistant in returning the Co2 for a product. Follow the directions provided:"""
 system_message_prompt = SystemMessagePromptTemplate.from_template(template)
-human_template = "{text}"
-human_message_prompt = HumanMessagePromptTemplate.from_template(human_template)
-chat_prompt = ChatPromptTemplate.from_messages([system_message_prompt, human_message_prompt])
+#human_template = "{text}"
+#human_message_prompt = HumanMessagePromptTemplate.from_template(human_template)
+chat_prompt = ChatPromptTemplate.from_messages([system_message_prompt])
 def get_chatassistant_aitopics():
     aitopics = LLMChain(
-        llm=ChatPerplexity(model="llama-3.1-sonar-huge-128k-online", temperature=0,max_tokens=4000),prompt=chat_prompt,verbose=True)
+        llm=ChatPerplexity(model="llama-3.1-sonar-huge-128k-online", temperature=0,max_tokens=4000),prompt=card,verbose=True)
     return aitopics
 aitopics = get_chatassistant_aitopics()
 
-responsememories = aitopics.run(card)
+responsememories = aitopics.run()
 st.warning("Current Product:  \n"+responsememories)
