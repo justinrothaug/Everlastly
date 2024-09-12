402382101046d3d6ed1d11de8953f123ea3f1cbd
justinrothaug <94494722+justinrothaug@users.noreply.github.com>
2024-09-12 14:38:56 -0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -52,111 +52,109 @@ st.markdown(
     unsafe_allow_html=True
 )
 
-with st.sidebar: 
-      text_input = st.text_input("Enter Everlastly Product 👇", key="4", value = "ZWILLING Twin Kitchen Shears, 8 Inch")
 
-      product_prompt = st.text_area("Product & BOM 👇", key="5", value = "Follow the below steps:\n"+
-      "*Prioritize sources like Amazon and the Company website*\n"+
-      "1) Identify the Total Weight of the product.\n"+
-      "2) Find every material used in creating the product. Do not skip any materials.Materials combined must equal 100%.\n"+
-      "3) For each material, find both the source country and manufacturing country.\n"+
-      "4) Find the *estimated % of the total volume for each material. \n"+
-      "5) Find the density for each material in lb/ft^3. List the source for this data\n"+
-      "6) Calculate the weight of each material using the following steps:\n"+
-      "6a) find the Volume-Density by multiplying each material’s volume percentage by that material’s density\n"+
-      "6b) Calculate the Density-Ratio by dividing that material’s Volume-Density by the total for all Volume-Densities of all materials found in the product\n"+
-      "6c) Multiply the Density Ratio by the total weight of the product. This should equal the total weight of the product found in Step 1.\n"+                         
-      "7) Find the Published Carbon Footprint for each Material, expressed as Co2 per Pound\n"+
-      "8) Multiply the Part Weight of each material by its respective Published Carbon Footprint per pound to get the Carbon Footprint for that Material Part. Then sum these values to estimate the Total Co2 for the Product.\n"+
-      "Only output in Table format like this:\n"+ 
-      "UPC Code,Product Name,Material,Source Country, Manufacturing Country,Estimated % in Volume,Material Density, Volume Density, Density Ratio,Weight in Pounds,Published CO2 Carbon Footprint per Pound,Total Carbon Footprint for that Material Part\n"+
-      "List the Data Source URL links (Amazon, ect.)")
+with st.sidebar: 
+      message = st.text_input("Enter Everlastly Product 👇", key="4", value = "ZWILLING Twin Kitchen Shears, 8 Inch")
+      #st.button('Run🍃', on_click=ProductBOM, key = "121", use_container_width=True)
+
+
+product_prompt = st.text_area("Product & BOM 👇", key="5", value = "Follow the below steps:\n"+
+"*Prioritize sources like Amazon and the Company website*\n"+
+"1) Identify the Total Weight of the product.\n"+
+"2) Find every material used in creating the product. Do not skip any materials.Materials combined must equal 100%.\n"+
+"3) For each material, find both the source country and manufacturing country.\n"+
+"4) Find the *estimated % of the total volume for each material. \n"+
+"5) Find the density for each material in lb/ft^3. List the source for this data\n"+
+"6) Calculate the weight of each material using the following steps:\n"+
+"6a) find the Volume-Density by multiplying each material’s volume percentage by that material’s density\n"+
+"6b) Calculate the Density-Ratio by dividing that material’s Volume-Density by the total for all Volume-Densities of all materials found in the product\n"+
+"6c) Multiply the Density Ratio by the total weight of the product. This should equal the total weight of the product found in Step 1.\n"+                         
+"7) Find the Published Carbon Footprint for each Material, expressed as Co2 per Pound\n"+
+"8) Multiply the Part Weight of each material by its respective Published Carbon Footprint per pound to get the Carbon Footprint for that Material Part. Then sum these values to estimate the Total Co2 for the Product.\n"+
+"Only output in Table format like this:\n"+ 
+"UPC Code,Product Name,Material,Source Country, Manufacturing Country,Estimated % in Volume,Material Density, Volume Density, Density Ratio,Weight in Pounds,Published CO2 Carbon Footprint per Pound,Total Carbon Footprint for that Material Part\n"+
+"List the Data Source URL links (Amazon, ect.)")
+
+product_prompt2 = st.text_area("Product & BOM 👇", key="5", value = "Follow the below steps:\n"+
+"*Prioritize sources like Amazon and the Company website*\n"+
+"1) Identify the Total Weight of the product.\n"+
+"2) Find every material used in creating the product. Do not skip any materials.Materials combined must equal 100%.\n"+
+"3) For each material, find both the source country and manufacturing country.\n"+
+"4) Find the *estimated % of the total volume for each material. \n"+
+"5) Find the density for each material in lb/ft^3. List the source for this data\n"+
+"6) Calculate the weight of each material using the following steps:\n"+
+"6a) find the Volume-Density by multiplying each material’s volume percentage by that material’s density\n"+
+"6b) Calculate the Density-Ratio by dividing that material’s Volume-Density by the total for all Volume-Densities of all materials found in the product\n"+
+"6c) Multiply the Density Ratio by the total weight of the product. This should equal the total weight of the product found in Step 1.\n"+                         
+"7) Find the Published Carbon Footprint for each Material, expressed as Co2 per Pound\n"+
+"8) Multiply the Part Weight of each material by its respective Published Carbon Footprint per pound to get the Carbon Footprint for that Material Part. Then sum these values to estimate the Total Co2 for the Product.\n"+
+"Only output in Table format like this:\n"+ 
+"UPC Code,Product Name,Material,Source Country, Manufacturing Country,Estimated % in Volume,Material Density, Volume Density, Density Ratio,Weight in Pounds,Published CO2 Carbon Footprint per Pound,Total Carbon Footprint for that Material Part\n"+
+"List the Data Source URL links (Amazon, ect.)")
+
+
    
-      category_prompt = st.text_area("Category 👇", key="3", value = "Follow the below steps:\n"+
-      "Create an Excel style table of product categories based on the Amazon listing,  using the following formatting as a guide: \n"+
-      "Department, Category, Subcategory, Specific Category. \n"+ 
-      "Add a final column that shows the type of product within the specific category by analyzing the different product types available.\n"+
-      "Hide the explanation")
-      
-      lifespan_prompt = st.text_area("Lifespan 👇", key="2", value = "Follow the below steps:\n"+
-      "For the general types of this Product's Category, estimate their lifespan in one number of years where lifetime is equal to 100 years, and display results in an table format including a column that displays the specific product category.\n"+
-      "Hide the steps and explanation")
-    
 
+st.warning(text_input)
 
-productcard="For the following product:"+text_input+product_prompt
-categorycard="For the following product:"+text_input+category_prompt
-lifespancard="For the following: "+text_input+lifespan_prompt
-      
+def eval_output(message):
+     productcard="For the following product:"+text_input+product_prompt
+     template = """You are a helpful assistant in following instructions for {text}. 
+     Provide a one sentence explanation """
+     system_message_prompt = SystemMessagePromptTemplate.from_template(template)
+     human_template = "{text}"
+     human_message_prompt = HumanMessagePromptTemplate.from_template(human_template)
+     chat_prompt = ChatPromptTemplate.from_messages([system_message_prompt, human_message_prompt])
+     def get_chatassistant_aitopics():
+         aitopics = LLMChain(
+             llm=ChatPerplexity(model="llama-3.1-sonar-small-128k-online", temperature=0),prompt=chat_prompt,verbose=True)
+         return aitopics
+     aitopics = get_chatassistant_aitopics()
+     responsememories = aitopics.run(productcard)
 
-def ProductBOM():
+def production_output(message):
+     productcard="For the following product:"+text_input+product_prompt2
+     template = """You are a helpful assistant in following instructions for {text}. 
+     Provide a one sentence explanation """
+     system_message_prompt = SystemMessagePromptTemplate.from_template(template)
+     human_template = "{text}"
+     human_message_prompt = HumanMessagePromptTemplate.from_template(human_template)
+     chat_prompt = ChatPromptTemplate.from_messages([system_message_prompt, human_message_prompt])
+     def get_chatassistant_aitopics():
+         aitopics = LLMChain(
+             llm=ChatPerplexity(model="llama-3.1-sonar-small-128k-online", temperature=0),prompt=chat_prompt,verbose=True)
+         return aitopics
+     aitopics = get_chatassistant_aitopics()
+     responsememories = aitopics.run(productcard)
+   
+#STREAMLIT APP
+#st.title("💬 RealAvatar Arena")
+#message = st.text_input("Let's see which AI is better. Write something:")
+if message:
+    first_one = random.choice([1,2,3,4,5,6])
+    if first_one>3:
+        response1 = eval_output(message)
+        response2 = production_output(message)
+    else:
+        response1 = production_output(message)
+        response2 = eval_output(message)
 
-  st.warning(text_input)
+    col1, col2 = st.columns(2)
+    with col1:
+        st.success(response1)
+        if st.button("Select Bot 1 Response", key='select1'):
+            st.session_state.selected = "Bot 1"
+    with col2:
+        st.success(response2)
+        if st.button("Select Bot 2 Response", key='select2'):
+            st.session_state.selected = "Bot 2"
 
-    
-  template = """You are a helpful assistant in following instructions for {text}. 
-  Provide a one sentence explanation """
-  system_message_prompt = SystemMessagePromptTemplate.from_template(template)
-  human_template = "{text}"
-  human_message_prompt = HumanMessagePromptTemplate.from_template(human_template)
-  chat_prompt = ChatPromptTemplate.from_messages([system_message_prompt, human_message_prompt])
-  def get_chatassistant_aitopics():
-      aitopics = LLMChain(
-          llm=ChatPerplexity(model="llama-3.1-sonar-small-128k-online", temperature=0),prompt=chat_prompt,verbose=True)
-      return aitopics
-  aitopics = get_chatassistant_aitopics()
-  responsememories = aitopics.run(productcard)
-  st.warning("Product BOM:  \n"+responsememories)
-
-
-#def category():
-  template = """You are a helpful assistant in following instructions for {text}. 
-  Provide a one sentence explanation """
-  system_message_prompt = SystemMessagePromptTemplate.from_template(template)
-  human_template = "{text}"
-  human_message_prompt = HumanMessagePromptTemplate.from_template(human_template)
-  chat_prompt = ChatPromptTemplate.from_messages([system_message_prompt, human_message_prompt])
-  def get_chatassistant_aitopics():
-      aitopics = LLMChain(
-          llm=ChatPerplexity(model="llama-3.1-sonar-small-128k-online", temperature=0),prompt=chat_prompt,verbose=True)
-      return aitopics
-  aitopics = get_chatassistant_aitopics()
-  responsememories = aitopics.run(categorycard)
-  st.warning("Product Category:  \n"+responsememories)
-
-#def lifespan():
-  template = """You are a helpful assistant in following instructions for {text}. 
-  Provide a one sentence explanation """
-  system_message_prompt = SystemMessagePromptTemplate.from_template(template)
-  human_template = "{text}"
-  human_message_prompt = HumanMessagePromptTemplate.from_template(human_template)
-  chat_prompt = ChatPromptTemplate.from_messages([system_message_prompt, human_message_prompt])
-  def get_chatassistant_aitopics():
-      aitopics = LLMChain(
-          llm=ChatPerplexity(model="llama-3.1-sonar-small-128k-online", temperature=0),prompt=chat_prompt,verbose=True)
-      return aitopics
-  aitopics = get_chatassistant_aitopics()
-  responsememories = aitopics.run(lifespancard)
-  st.warning("Product Lifespan:  \n"+responsememories)
-
-
-
-with st.sidebar: 
-    st.button('Run🍃', on_click=ProductBOM, key = "121", use_container_width=True)
-
-
-
-
-
-#Test output from API directly:
-def StartConvo2():
-  import requests
-  url = "https://api.perplexity.ai/chat/completions"
-  payload = {"model": "llama-3.1-sonar-small-128k-online","messages": [{"role": "system","content": "Be as accurate as possible"},{"role": "user","content": card}]}
-  headers = {"accept": "application/json", "content-type": "application/json", "authorization": PPLX_API_KEY2}
-  response = requests.post(url, json=payload, headers=headers)
-  responsetext=response.text
-  st.warning("Current Product:  \n"+responsetext)
-#st.button('Run2', on_click=StartConvo2, key = "120", use_container_width=True)
+    if 'selected' in st.session_state:
+        if first_one>3:
+            st.success(f"You selected Llama 3.1 70B")
+        else:
+            st.success(f"You selected Claude 3.5 Sonnet")
+           
+  #st.warning("Product BOM:  \n"+responsememories)
 
 
